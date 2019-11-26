7c04fcc494deb0072d4edfc4775dbb1d1e372db5
Rene Pupo <rvpupo@gmail.com>
2019-11-26 09:00:22 -0500

    Update server.js
    make it mine.

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const port = 80
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Hello World\n')
-  response.end('Version: ' + process.env.NODE_VERSION + '\n')
+  response.write('Ray :: Hello World\n')
+  response.end('Ray :: Version: ' + process.env.NODE_VERSION + '\n')
 })
 
 server.listen(port)
 
-console.log(`Server running at http://localhost: ${port}`)
+console.log(`Ray :: Server running at http://localhost: ${port}`)
