500eabf7b3bc671afc77e8343328cbf13ee908cb
Rene Pupo <rvpupo@gmail.com>
2019-11-27 14:01:31 +0000

    testing

--- server.js
+++ server.js
@@ -1,13 +1,17 @@
 const http = require('http')
 
 const port = 80
+const start = Date.now();
 
 const server = http.createServer((request, response) => {
+  const millis = Date.now() - start;
   response.writeHead(200, {'Content-Type': 'text/plain'})
   response.write('Ray :: Hello World\n')
-  response.end('Ray :: Version: ' + process.env.NODE_VERSION + '\n')
+  response.end('Ray :: Elapsed milli-secs : ' + millis + ' Version: ' + process.env.NODE_VERSION + '\n')
+
 })
 
 server.listen(port)
 
 console.log(`Ray :: Server running at http://localhost: ${port}`)
+
