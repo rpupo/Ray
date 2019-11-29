bfc2089feffcc4043a06cf72cd6cd17bdf61da1f
Rene Pupo <rvpupo@gmail.com>
2019-11-29 19:38:31 +0000

    Ray -> RVP

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const start = Date.now();
 const server = http.createServer((request, response) => {
   const millis = Date.now() - start;
   response.writeHead(200, {'Content-Type': 'text/plain'})
-  response.write('Ray :: Hello World\n')
-  response.end('Ray :: Elapsed milli-secs : ' + millis + ' Version: ' + process.env.NODE_VERSION + '\n')
+  response.write('RVP :: Hello World\n')
+  response.end('RVP :: Elapsed milli-secs : ' + millis + ' Version: ' + process.env.NODE_VERSION + '\n')
 
 })
 
 server.listen(port)
 
-console.log(`Ray :: Server running at http://localhost: ${port}`)
+console.log(`RVP:: Server running at http://localhost: ${port}`)
 
